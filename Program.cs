3b427bd091b8842b6833966dcf529b78c7472f75
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-11-16 13:21:00 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void PickUp()
+        {
+
+        }
+        static void Drop()
+        {
+
+        }
+        static void Use()
+        {
+
+        }
+        static void ClearInventory()
+        {
+
+        }
+        static void ShowInventory()
+        {
+
+        }
+    }
+}
